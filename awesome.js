89673220a88a6e83777a3b49528e13b21ce9953f
aaronxzxzxx <aaronchotzenjenner@gmail.com>
2017-06-09 10:38:59 -0700

    Added Links

--- awesome.js
+++ awesome.js
@@ -20,7 +20,7 @@ const app = new Vue({
 		
 		new Post(
 				'First Post: Neuer Gloves',
-				'https://vuejs.org',
+				'https://www.soccer.com',
 				'Aaron',
 				'https://s7d2.scene7.com/is/image/dkscdn/16ADIACTRNSPRMNBBSCG_Blue_Black_is/',
 				 'I like the neuer gloves because the offer the best control and have a good contact latex.'
@@ -28,7 +28,7 @@ const app = new Vue({
 
 			new Post(
 				'Second Post:Premier SGT',
-				'https://vuejs.org',
+				'https://www.soccer.com',
 				'Jamie',
 				'http://www.prodirectsoccer.com/productimages/V3_1_Main/150352.jpg',
 				'The Premier SGTs feature laser cut grooves for better grip but the latex is not very durable for the premium price you are paying'
@@ -36,7 +36,7 @@ const app = new Vue({
 
 			new Post(
 				'Third Post: Spiderman Gloves',
-				'https://google.com',
+				'https://www.soccer.com',
 				'Jack',
 				'https://s-media-cache-ak0.pinimg.com/236x/09/0f/b0/090fb0853ce6a74ebbfea8fba7d723ed.jpg',
 				'I like spiderman.'
@@ -44,7 +44,7 @@ const app = new Vue({
 
 			new Post(
 				'Fourth Post: Oven Mitts',
-				'https://vuejs.org',
+				'https://www.soccer.com',
 				'Mom',
 				'https://s-media-cache-ak0.pinimg.com/originals/f7/e2/45/f7e2450231194472320127a3dbf6ba41.jpg',
 				'These gloves feature great grip on hot trays for my casserole'
@@ -52,7 +52,7 @@ const app = new Vue({
 
 			new Post(
 				'Fifth Post: Walmart Gloves',
-				'https://google.com',
+				'https://www.soccer.com',
 				'Maxx',
 				'https://images-na.ssl-images-amazon.com/images/I/815Kv48Y7vL._SY355_.jpg',
 				'This is the official Walmart goalie glove, the best in the world.'
